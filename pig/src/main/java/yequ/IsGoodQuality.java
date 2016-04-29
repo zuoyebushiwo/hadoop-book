ace86326e2385ed2b3f6df7a365a752b37c22632
zuoyebushiwo <junxuezhang@yeah.net>
2016-04-29 17:38:21 +0800

    131

---
+++ pig/src/main/java/yequ/IsGoodQuality.java
@@ -0,0 +1,19 @@
+package yequ;
+
+import org.apache.pig.FilterFunc;
+import org.apache.pig.data.Tuple;
+
+import java.io.IOException;
+
+/**
+ * @author ZuoYe
+ * @date 2016年04月29
+ */
+public class IsGoodQuality extends FilterFunc {
+
+
+    @Override
+    public Boolean exec(Tuple input) throws IOException {
+        return null;
+    }
+}
